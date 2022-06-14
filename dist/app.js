a9f20db043ecb0b90d14eb1fe02c4c56ae0f6aba
Turt3l <77460157+Turt3l@users.noreply.github.com>
2022-06-14 17:31:33 +0300

    almost completed mobile view

--- dist/app.js
+++ dist/app.js
@@ -7,7 +7,52 @@
   \***********************/
 /***/ (() => {
 
+function _createForOfIteratorHelper(o, allowArrayLike) { var it = typeof Symbol !== "undefined" && o[Symbol.iterator] || o["@@iterator"]; if (!it) { if (Array.isArray(o) || (it = _unsupportedIterableToArray(o)) || allowArrayLike && o && typeof o.length === "number") { if (it) o = it; var i = 0; var F = function F() {}; return { s: F, n: function n() { if (i >= o.length) return { done: true }; return { done: false, value: o[i++] }; }, e: function e(_e) { throw _e; }, f: F }; } throw new TypeError("Invalid attempt to iterate non-iterable instance.\nIn order to be iterable, non-array objects must have a [Symbol.iterator]() method."); } var normalCompletion = true, didErr = false, err; return { s: function s() { it = it.call(o); }, n: function n() { var step = it.next(); normalCompletion = step.done; return step; }, e: function e(_e2) { didErr = true; err = _e2; }, f: function f() { try { if (!normalCompletion && it["return"] != null) it["return"](); } finally { if (didErr) throw err; } } }; }
 
+function _unsupportedIterableToArray(o, minLen) { if (!o) return; if (typeof o === "string") return _arrayLikeToArray(o, minLen); var n = Object.prototype.toString.call(o).slice(8, -1); if (n === "Object" && o.constructor) n = o.constructor.name; if (n === "Map" || n === "Set") return Array.from(o); if (n === "Arguments" || /^(?:Ui|I)nt(?:8|16|32)(?:Clamped)?Array$/.test(n)) return _arrayLikeToArray(o, minLen); }
+
+function _arrayLikeToArray(arr, len) { if (len == null || len > arr.length) len = arr.length; for (var i = 0, arr2 = new Array(len); i < len; i++) { arr2[i] = arr[i]; } return arr2; }
+
+var list = document.querySelectorAll('.language-choice');
+
+var _iterator = _createForOfIteratorHelper(list),
+    _step;
+
+try {
+  for (_iterator.s(); !(_step = _iterator.n()).done;) {
+    var language = _step.value;
+    language.addEventListener("click", toggleMenu);
+  }
+} catch (err) {
+  _iterator.e(err);
+} finally {
+  _iterator.f();
+}
+
+function toggleMenu() {
+  var x = this.parentNode.querySelector(".languages");
+  var x1 = this;
+
+  if (x.style.display == "none") {
+    this.classList.add("active");
+    x.style.display = "block";
+  } else {
+    this.classList.remove("active");
+    x.style.display = "none";
+  }
+}
+
+var modal = document.getElementById("Modal");
+var span = document.getElementsByClassName("close")[0];
+var button = document.getElementById("openModal");
+
+button.onclick = function () {
+  modal.style.display = "block";
+};
+
+span.onclick = function () {
+  modal.style.display = "none";
+};
 
 /***/ }),
 
---
+++ src/js/app.js
@@ -0,0 +1,25 @@
+const list = document.querySelectorAll('.language-choice');
+for (let language of list) {
+    language.addEventListener("click", toggleMenu);
+}
+function toggleMenu(){
+
+    var x = this.parentNode.querySelector(".languages");
+    var x1 = this
+    if (x.style.display == "none") {
+        this.classList.add("active")
+        x.style.display = "block";
+    } else {
+        this.classList.remove("active")
+        x.style.display = "none";
+    }
+}
+var modal = document.getElementById("Modal");
+var span = document.getElementsByClassName("close")[0];
+var button = document.getElementById("openModal");
+button.onclick = function(){
+    modal.style.display = "block";
+}
+span.onclick = function() {
+    modal.style.display = "none";
+  }
