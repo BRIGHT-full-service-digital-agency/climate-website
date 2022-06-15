1322a2ff8f1c8ce28d8425f33b1b919d18d8d451
Turt3l <77460157+Turt3l@users.noreply.github.com>
2022-06-15 17:48:19 +0300

    almost completed website

--- src/js/app.js
+++ src/js/app.js
@@ -1,3 +1,5 @@
+import Swiper, { Navigation, Pagination, Scrollbar } from 'swiper';
+
 const list = document.querySelectorAll('.language-choice');
 for (let language of list) {
     language.addEventListener("click", toggleMenu);
@@ -7,7 +9,6 @@ function toggleMenu(){
     var x = this.parentNode.querySelector(".languages");
     var x1 = this
     if (x.style.display == "none") {
-
         this.classList.add("active")
         x.style.display = "block";
     } else {
@@ -25,4 +26,66 @@ button.onclick = function(){
 span.onclick = function() {
     modal.style.display = "none";
     document.querySelector('html').classList.remove("modal-open");
-  }
+}
+
+var backToTop = document.getElementById("toTop");
+backToTop.onclick = function(){
+    window.scrollTo(1, 1);
+}
+var rotate = document.getElementById("arrow1");
+var rotate1 = document.getElementById("arrow2");
+var rotate2 = document.getElementById("arrow3");
+
+//document.addEventListener("DOMContentLoaded", rotateArrow);
+
+//function rotateArrow(){
+    rotate.animate([
+        { transform: 'rotate(360deg)' },
+      ], {
+        duration: 10000,
+        iterations: Infinity
+      });
+    rotate1.animate([
+        { transform: 'rotate(360deg)' }
+      ], {
+        duration: 10000,
+        iterations: Infinity
+      });
+      rotate2.animate([
+        { transform: 'rotate(360deg)' }
+      ], {
+        duration: 10000,
+        iterations: Infinity
+      });
+//}
+
+
+
+
+
+
+
+
+
+
+const swiper = new Swiper('.swiper', {
+    modules: [ Navigation, Pagination, Scrollbar ],
+    freeMode: true,
+    direction: 'horizontal',
+    breakpoints: {
+      1200: {
+        slidesPerView: 3,
+        spaceBetween: 30,
+      },
+      850: {
+        slidesPerView: 2
+      },
+      320: {
+        slidesPerView: 1
+      }
+
+    },
+    scrollbar: {
+        el: ".swiper-scrollbar",
+      },
+  });
